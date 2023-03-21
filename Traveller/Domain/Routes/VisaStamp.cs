1a8f305ac47e832abcfa07d21eb204e22e2133d5
david <davidlorente78@hotmail.com>
2023-03-21 14:48:32 +0100

    Itinerary Draft

---
+++ Traveller/Domain/Routes/VisaStamp.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Generic;
+using Traveller.Domain;
+
+namespace Domain.Routes
+{
+    public class VisaStamp : Entity
+    {
+        public Visa Visa;
+        public Country Country { get; set; }
+        public BorderCrossing BorderCrossing { get; set; }
+        public BorderCrossing? ExitPoint { get; set; }
+
+
+        public DateTime IssueDate { get; set; }
+        public DateTime ExpiryDate { get; set; }
+
+
+        public DateTime EntryDate { get; set; }
+        public DateTime ExitDate { get; set; }
+
+
+        public DateTime ValidUntill { get; set; }
+        public DateTime ValidFrom { get; set; }
+    }
+}
---
+++ Traveller/StaticData/Itinerary/DataItinerary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Routes;
+using Traveller.Domain;
+using Traveller.StaticData;
+
+namespace Data.Itinerary
+{
+    internal class DataItinerary
+    {
+        public static Domain.Routes.Itinerary Itinerary = new Domain.Routes.Itinerary
+        {
+            Name = "Best of South East Asia in 21 days",
+            Description = "",
+            StartMonth = 11,
+            EndMonth = 12,
+            ItineraryDestinations = new List<Destination>
+            { ThailandDestinations.Bangkok,
+            LaosDestinations.LuangPrabang,
+            NepalDestinations.Kathmandu}
+
+
+
+        };
+    }
+}
+
+
